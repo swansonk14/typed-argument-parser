613a03984c8daa4c365fe528cf4fe04dcd26e459
Kyle Swanson <swansonk.14@gmail.com>
2019-07-19 23:40:12 -0400

    Adding tests

---
+++ tests/integration.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest import TestCase
+
+
+class IntegrationTests(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unit.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest import TestCase
+
+
+class UnitTests(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
