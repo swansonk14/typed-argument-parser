ca776acedd7495e222ddd39895fe5c8cb3985217
jmmichel <jmmichel@mit.edu>
2019-07-25 02:29:44 -0400

    tests and hardening for the parser

--- tap/parse_docstrings.py
+++ tap/parse_docstrings.py
@@ -5,6 +5,7 @@ import re
 def extract_descriptions(doc: Optional[str]) -> (Optional[str], Optional[Dict[str, str]]):
     """Extracts the class and variable descriptions from a class-level doc string.
 
+    Attributes:
     :param doc: The docstring of a namespace class
     :return: a tuple of the description of the class
         and a dictionary mapping each attribute to its description
@@ -13,20 +14,30 @@ def extract_descriptions(doc: Optional[str]) -> (Optional[str], Optional[Dict[st
         return None, None
 
     # Extract the description from the header
-    general_description, attributes_block = doc.split('\n\n', 1)
-    general_description = re.sub(' +', ' ', general_description.replace('\t', '').replace('\n', ' '))
+    try:
+        # Split on the attributes keyword
+        general_description, attributes_block = re.split(r'[\t| ]*\n+[\t| ]*Attributes:', doc, maxsplit=1)
+    except ValueError:
+        raise ValueError("""The documentation should be of the form:
 
-    # TODO: This assumes that ':' isn't in the description. Can be cleaned up with better regex.
+        Description
+
+        Attributes:
+        :arg1: Description
+        :arg2: Description""")
+    general_description = re.sub(' +', ' ', general_description.replace('\t', ''), count=1).replace('  ', '').strip()
+
     # Extract the attributes and descriptions
-    enumerated_attributes = attributes_block.replace('\t', '').split(':')[2:]
     attributes = [
-            re.sub(' +', ' ', attribute.replace('\n', ''))
-            for attribute in enumerated_attributes[::2]
+        s.replace('  ', '').strip()
+        for s in re.findall(':(\w*):', attributes_block)
         ]
+
     descriptions = [
-            re.sub(' +', ' ', attribute)[1:-1]
-            for attribute in enumerated_attributes[1::2]
+        s.replace('  ', '').strip()
+        for s in re.split('[\n ]*:\w*: ', attributes_block)[1:]
         ]
+
     attribute_descriptions = dict(zip(attributes, descriptions))
 
     return general_description, attribute_descriptions
---
+++ tests/test_docstring_parsing.py
@@ -0,0 +1,90 @@
+import unittest
+from unittest import TestCase
+
+from tap.parse_docstrings import extract_descriptions
+
+
+class DocstringParsingTests(TestCase):
+
+    def test_simple_docstring(self) -> None:
+        simplest_docstring = """How simple is this docstring?
+
+        Attributes:
+        :pretty_simple: this is quite simple
+        :very_simple: quite as simple as it gets
+        """
+        gen_desc, attr_desc = extract_descriptions(simplest_docstring)
+        self.assertEqual(gen_desc, "How simple is this docstring?")
+        self.assertEqual(attr_desc, {
+            "pretty_simple": "this is quite simple",
+            "very_simple": "quite as simple as it gets",
+            })
+
+    def test_with_extract_tab(self) -> None:
+        extra_tabs = """Extra tab.\n\t \nAttributes:\n:arg: desc"""
+        gen_desc, attr_desc = extract_descriptions(extra_tabs)
+        self.assertEqual(gen_desc, "Extra tab.")
+        self.assertEqual(attr_desc, {"arg": "desc"})
+
+    def test_multiline_descriptions(self) -> None:
+        long_descriptions = """Longer description with an unnecessary tabs.
+
+        More of a description
+        Attributes:
+        :arg: far
+        too
+        many enters
+        :more_args: trailing enters
+
+            at the end
+
+         """
+        gen_desc, attr_desc = extract_descriptions(long_descriptions)
+        self.assertEqual(gen_desc, "Longer description with an unnecessary tabs.\n\nMore of a description")
+        self.assertEqual(attr_desc, {
+            "arg": "far\ntoo\nmany enters",
+            "more_args": "trailing enters\n\nat the end",
+            })
+
+    def test_no_attribute_errors(self) -> None:
+        no_attributes = """The comment lacks attributes, which should error out."""
+        with self.assertRaises(ValueError) as cm:
+            extract_descriptions(no_attributes)
+
+    def test_colons_in_description(self) -> None:
+        colons_in_description = """So happy :)
+
+        Attributes:
+        :arg: still so happy :)
+        """
+        gen_desc, attr_desc = extract_descriptions(colons_in_description)
+        self.assertEqual(gen_desc, "So happy :)")
+        self.assertEqual(attr_desc, {"arg": "still so happy :)"})
+
+    def test_sneaky_attributes(self) -> None:
+        colons_in_description = """Attributes: So happy :) Attributes:
+
+        Attributes:
+        :arg: still so happy :) Attributes:
+        :another_arg: the arg
+        """
+        gen_desc, attr_desc = extract_descriptions(colons_in_description)
+        self.assertEqual(gen_desc, "Attributes: So happy :) Attributes:")
+        self.assertEqual(attr_desc, {
+            'arg': 'still so happy :) Attributes:',
+            'another_arg': 'the arg'
+            })
+
+    # A debatable example.
+    # def test_no_description(self) -> None:
+    #     no_main_description = """
+    #     Arguments:
+    #     :arg: the first arg
+    #     """
+    #     gen_desc, attr_desc = extract_descriptions(no_main_description)
+    #     self.assertEqual(gen_desc, "")
+    #     self.assertEqual(attr_desc, {"arg": "the first arg"})
+
+
+if __name__ == '__main__':
+    unittest.main()
