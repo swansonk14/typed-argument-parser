ade3d55a52d3df93cda12c484dc00eb974cfa5ef
Jesse Michel <jmmichel@mit.edu>
2023-02-18 16:01:42 -0500

    Deprecate python 3.6 and add 3.11

--- setup.py
+++ setup.py
@@ -34,11 +34,11 @@ setup(
     tests_require=['pytest'],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         "Typing :: Typed"
