74ce8c34fa3295210a4e697a0b0537fc97fb6642
Kyle Swanson <swansonk.14@gmail.com>
2023-07-12 15:57:18 -0700

    Initial failing attempt at convert_to_tap

--- tap/__init__.py
+++ tap/__init__.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentError, ArgumentTypeError
 from tap._version import __version__
 from tap.tap import Tap
-from tap.tapify import tapify
+from tap.tapify import convert_to_tap, tapify
 
-__all__ = ['ArgumentError', 'ArgumentTypeError', 'Tap', 'tapify', '__version__']
+__all__ = ['ArgumentError', 'ArgumentTypeError', 'Tap', 'convert_to_tap', 'tapify', '__version__']
--- tap/tapify.py
+++ tap/tapify.py
@@ -1,13 +1,85 @@
 """Tapify module, which can initialize a class or run a function by parsing arguments from the command line."""
 from inspect import signature, Parameter
-from typing import Any, Callable, List, Optional, TypeVar, Union
+from typing import Any, Callable, List, Optional, Type, TypeVar, Union
 
 from docstring_parser import parse
 
 from tap import Tap
+from tap.utils import type_to_str
 
 InputType = TypeVar('InputType')
 OutputType = TypeVar('OutputType')
+
+
+def convert_to_tap(class_or_function: Union[Callable[[InputType], OutputType], OutputType]) -> Type[Tap]:
+    """Converts a class or function into a Tap class.
+
+    :param class_or_function: The class or function whose parameters will be converted to a Tap class.
+    :return: A Tap class with arguments from the function.
+    """
+    # Get signature from class or function
+    sig = signature(class_or_function)
+
+    # Parse class or function docstring in one line
+    if isinstance(class_or_function, type) and class_or_function.__init__.__doc__ is not None:
+        doc = class_or_function.__init__.__doc__
+    else:
+        doc = class_or_function.__doc__
+
+    # Parse docstring
+    docstring = parse(doc)
+
+    # Get class or function description
+    description = '\n'.join(filter(None, (docstring.short_description, docstring.long_description)))
+
+    # Add arguments of function to the Tap class
+    annotations, defaults = {}, {}
+
+    # Add arguments of class init or function to the Tap object
+    for param_name, param in sig.parameters.items():
+        # Skip **kwargs
+        if param.kind == Parameter.VAR_KEYWORD:
+            continue
+
+        # Get type of the argument
+        if param.annotation != Parameter.empty:
+            # Any type defaults to str (needed for dataclasses where all non-default attributes must have a type)
+            if param.annotation is Any:
+                annotations[param.name] = str
+            # Otherwise, get the type of the argument
+            else:
+                annotations[param.name] = param.annotation
+
+        # Get the default for the argument
+        if param.default != Parameter.empty:
+            defaults[param_name] = param.default
+
+    # Get docstring descriptions for parameters
+    param_to_docstring = {param.arg_name: param.description for param in docstring.params}
+
+    # Set parameter docstrings in configure
+    def configure(self):
+        for param_name in sig.parameters:
+            req_status = f'default={defaults[param_name]}' if param_name in defaults else 'required'
+
+            if param_name in annotations:
+                help = f'({type_to_str(annotations[param_name])}, {req_status})'
+            else:
+                help = f'({req_status})'
+
+            if param_name in param_to_docstring:
+                help += f' {param_to_docstring[param_name]}'
+
+            self.add_argument(f'--{param_name}', help=help)
+
+    # Create tap class
+    tap_class = type(f'{class_or_function.__name__}_tap', (Tap,), {
+        '__doc__': description,
+        '__annotations__': annotations,
+        'configure': configure,
+    } | defaults)
+
+    return tap_class
 
 
 def tapify(class_or_function: Union[Callable[[InputType], OutputType], OutputType],
@@ -24,65 +96,26 @@ def tapify(class_or_function: Union[Callable[[InputType], OutputType], OutputTyp
                         parsing the command line arguments and overwrite the function defaults but
                         are overwritten by the parsed command line arguments.
     """
-    # Get signature from class or function
-    sig = signature(class_or_function)
-
-    # Parse class or function docstring in one line
-    if isinstance(class_or_function, type) and class_or_function.__init__.__doc__ is not None:
-        doc = class_or_function.__init__.__doc__
-    else:
-        doc = class_or_function.__doc__
-
-    # Parse docstring
-    docstring = parse(doc)
-
-    # Get the description of each argument in the class init or function
-    param_to_description = {param.arg_name: param.description for param in docstring.params}
-
-    # Create a Tap object with a description from the docstring of the function or class
-    tap = Tap(description='\n'.join(filter(None, (docstring.short_description, docstring.long_description))))
-
-    # Keep track of whether **kwargs was provided
-    has_kwargs = False
-
-    # Add arguments of class init or function to the Tap object
-    for param_name, param in sig.parameters.items():
-        tap_kwargs = {}
-
-        # Skip **kwargs
-        if param.kind == Parameter.VAR_KEYWORD:
-            has_kwargs = True
-            known_only = True
-            continue
-
-        # Get type of the argument
-        if param.annotation != Parameter.empty:
-            # Any type defaults to str (needed for dataclasses where all non-default attributes must have a type)
-            if param.annotation is Any:
-                tap._annotations[param.name] = str
-            # Otherwise, get the type of the argument
-            else:
-                tap._annotations[param.name] = param.annotation
-
-        # Get the default or required of the argument
-        if param.name in func_kwargs:
-            tap_kwargs['default'] = func_kwargs[param.name]
-            del func_kwargs[param.name]
-        elif param.default != Parameter.empty:
-            tap_kwargs['default'] = param.default
-        else:
-            tap_kwargs['required'] = True
-
-        # Get the help string of the argument
-        if param.name in param_to_description:
-            tap.class_variables[param.name] = {'comment': param_to_description[param.name]}
-
-        # Add the argument to the Tap object
-        tap._add_argument(f'--{param_name}', **tap_kwargs)
-
     # If any func_kwargs remain, they are not used in the function, so raise an error
     if func_kwargs and not known_only:
         raise ValueError(f'Unknown keyword arguments: {func_kwargs}')
+
+    # Get signature from class or function
+    sig = signature(class_or_function)
+
+    # Determine if there are any **kwargs
+    has_kwargs = False
+    for param_name, param in sig.parameters.items():
+        if param.kind == Parameter.VAR_KEYWORD:
+            known_only = True
+            has_kwargs = True
+            break
+
+    # Get Tap class for the class or function
+    tap_class = convert_to_tap(class_or_function)
+
+    # Instantiate tap object
+    tap = tap_class()
 
     # Parse command line arguments
     command_line_args = tap.parse_args(
