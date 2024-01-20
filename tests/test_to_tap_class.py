0dcb8646fb319129143889083366a2ea6b4fd8ad
kddubey <kushdubey63@gmail.com>
2024-01-19 20:21:33 -0800

    test more complex subclasser

--- tests/test_to_tap_class.py
+++ tests/test_to_tap_class.py
@@ -1,8 +1,7 @@
 """
 Tests `tap.to_tap_class`.
 
-TODO: test help message, test subclass_tap_weird, test with func_kwargs
-TODO: I might redesign this soon. It's not thorough yet.
+TODO: test help message, test with func_kwargs
 """
 import dataclasses
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
@@ -17,7 +16,7 @@ from tap import to_tap_class, Tap
 class _Args:
     """These are the argument names which every type of class or function must contain."""
 
-    arg_str: str = dataclasses.field(metadata=dict(description="some string"))
+    arg_int: int = dataclasses.field(metadata=dict(description="some integer"))
     arg_bool: bool = True
     arg_list: Optional[List[str]] = dataclasses.field(default=None, metadata=dict(description="some list of strings"))
 
@@ -26,7 +25,7 @@ def _monkeypatch_eq(cls):
     """Monkey-patches `cls.__eq__` to check that the attribute values are equal to a dataclass representation of them"""
 
     def _equality(self, other: _Args) -> bool:
-        return _Args(self.arg_str, self.arg_bool, self.arg_list) == other
+        return _Args(self.arg_int, self.arg_bool, self.arg_list) == other
 
     cls.__eq__ = _equality
     return cls
@@ -36,9 +35,9 @@ def _monkeypatch_eq(cls):
 # if not required)
 
 
-def function(arg_str: str, arg_bool: bool = True, arg_list: Optional[List[str]] = None) -> _Args:
+def function(arg_int: int, arg_bool: bool = True, arg_list: Optional[List[str]] = None) -> _Args:
     """
-    :param arg_str: some string
+    :param arg_int: some integer
     :param arg_list: some list of strings
     """
     return _Args(*locals().values())
@@ -46,12 +45,12 @@ def function(arg_str: str, arg_bool: bool = True, arg_list: Optional[List[str]] 
 
 @_monkeypatch_eq
 class Class:
-    def __init__(self, arg_str: str, arg_bool: bool = True, arg_list: Optional[List[str]] = None):
+    def __init__(self, arg_int: int, arg_bool: bool = True, arg_list: Optional[List[str]] = None):
         """
-        :param arg_str: some string
+        :param arg_int: some integer
         :param arg_list: some list of strings
         """
-        self.arg_str = arg_str
+        self.arg_int = arg_int
         self.arg_bool = arg_bool
         self.arg_list = arg_list
 
@@ -65,7 +64,7 @@ DataclassBuiltin = _Args
 class DataclassPydantic:
     """Dataclass (pydantic)"""
 
-    arg_str: str = pydantic.dataclasses.Field(description="some string")
+    arg_int: int = pydantic.dataclasses.Field(description="some integer")
     arg_bool: bool = pydantic.dataclasses.Field(default=True)
     arg_list: Optional[List[str]] = pydantic.Field(default=None, description="some list of strings")
 
@@ -74,9 +73,33 @@ class DataclassPydantic:
 class Model(pydantic.BaseModel):
     """Pydantic model"""
 
-    arg_str: str = pydantic.Field(description="some string")
+    arg_int: int = pydantic.Field(description="some integer")
     arg_bool: bool = pydantic.Field(default=True)
     arg_list: Optional[List[str]] = pydantic.Field(default=None, description="some list of strings")
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        function,
+        Class,
+        DataclassBuiltin,
+        DataclassBuiltin(
+            "to_tap_class will work on instances of data models (for free). It ignores the attribute values",
+            arg_bool=False,
+            arg_list=["doesn't", "matter"],
+        ),
+        DataclassPydantic,
+        DataclassPydantic(arg_int=1_000, arg_bool=False, arg_list=[]),
+        Model,
+        Model(arg_int=1_000, arg_bool=False, arg_list=[]),
+    ],
+)
+def data_model(request: pytest.FixtureRequest):
+    """
+    Same as class_or_function. Only difference is that data_model is parametrized while class_or_function is not.
+    """
+    return request.param
 
 
 # Define some functions which take a class or function and calls `tap.to_tap_class` on it to create a `tap.Tap`
@@ -84,12 +107,18 @@ class Model(pydantic.BaseModel):
 
 
 def subclass_tap_simple(class_or_function: Any) -> Type[Tap]:
-    return to_tap_class(class_or_function)  # plain subclass / do nothing
+    """
+    Plain subclass, does nothing extra.
+    """
+    return to_tap_class(class_or_function)
 
 
-# TODO: use this. Will need to change how the test is parametrized b/c the output will depend on using
-# subclass_tap_simple vs subclass_tap_weird
-def subclass_tap_weird(class_or_function):
+def subclass_tap_complex(class_or_function):
+    """
+    It's conceivable that someone has a data model, but they want to add more arguments or handling when running a
+    script.
+    """
+
     def to_number(string: str) -> Union[float, int]:
         return float(string) if "." in string else int(string)
 
@@ -105,62 +134,32 @@ def subclass_tap_weird(class_or_function):
         def process_args(self) -> None:
             # You can still validate and modify arguments
             if self.argument_with_really_long_name > 4:
-                raise ValueError("nope")
+                raise ValueError("argument_with_really_long_name cannot be > 4")
 
             # No auto-complete (and other niceties) for the super class attributes b/c this is a dynamic subclass. Sorry
-            if self.arg_bool:
-                self.arg_str += " processed"
+            if self.arg_bool and self.arg_list is not None:
+                self.arg_list.append("processed")
 
     return TapSubclass
 
 
-@pytest.mark.parametrize("subclasser", [subclass_tap_simple])
-@pytest.mark.parametrize(
-    "class_or_function",
-    [
-        function,
-        Class,
-        DataclassBuiltin,
-        DataclassBuiltin(
-            "to_tap_class works on instances of data models (for free). It ignores the attribute values",
-            arg_bool=False,
-            arg_list=["doesn't", "matter"],
-        ),
-        DataclassPydantic,
-        DataclassPydantic(arg_str="...", arg_bool=False, arg_list=[]),
-        Model,
-        Model(arg_str="...", arg_bool=False, arg_list=[]),
-    ],
-)
-@pytest.mark.parametrize(
-    "args_string_and_arg_to_expected_value",
-    [
-        (
-            "--arg_str test --arg_list x y z",
-            {"arg_str": "test", "arg_bool": True, "arg_list": ["x", "y", "z"]},
-        ),
-        (
-            "--arg_str test --arg_list x y z --arg_bool",
-            {"arg_str": "test", "arg_bool": False, "arg_list": ["x", "y", "z"]},
-        ),
-        # The rest are invalid argument combos. This fact is indicated by the 2nd elt being a BaseException instance
-        (
-            "--arg_list x y z --arg_bool",  # Missing required arg_str
-            SystemExit(),
-        ),
-    ],
-)
-def test_to_tap_class(
+# Test that the subclasser parses the args correctly or raises the correct error.
+# The subclassers are tested separately b/c the parametrizaiton of args_string_and_arg_to_expected_value depends on the
+# subclasser.
+
+
+def _test_subclasser(
     subclasser: Callable[[Any], Type[Tap]],
     class_or_function: Any,
     args_string_and_arg_to_expected_value: Tuple[str, Union[Dict[str, Any], BaseException]],
+    test_call: bool = True,
 ):
     args_string, arg_to_expected_value = args_string_and_arg_to_expected_value
     TapSubclass = subclasser(class_or_function)
     tap = TapSubclass(description="My description")
 
-    # args_string is an invalid argument combo
     if isinstance(arg_to_expected_value, BaseException):
+        # args_string is an invalid argument combo
         expected_exception = arg_to_expected_value.__class__
         expected_error_message = str(arg_to_expected_value) or None
         with pytest.raises(expected_exception=expected_exception, match=expected_error_message):
@@ -171,6 +170,86 @@ def test_to_tap_class(
     args = tap.parse_args(args_string.split())
     for arg, expected_value in arg_to_expected_value.items():
         assert getattr(args, arg) == expected_value
-    if callable(class_or_function):
+    if test_call and callable(class_or_function):
         result = class_or_function(**args.as_dict())
         assert result == _Args(**arg_to_expected_value)
+
+
+@pytest.mark.parametrize(
+    "args_string_and_arg_to_expected_value",
+    [
+        (
+            "--arg_int 1 --arg_list x y z",
+            {"arg_int": 1, "arg_bool": True, "arg_list": ["x", "y", "z"]},
+        ),
+        (
+            "--arg_int 1 --arg_list x y z --arg_bool",
+            {"arg_int": 1, "arg_bool": False, "arg_list": ["x", "y", "z"]},
+        ),
+        # The rest are invalid argument combos, as indicated by the 2nd elt being a BaseException instance
+        (
+            "--arg_list x y z --arg_bool",  # Missing required arg_int
+            SystemExit(),  # TODO: figure out how to get argparse's error message and test that it matches
+        ),
+        (
+            "--arg_int not_an_int --arg_list x y z --arg_bool",  # Wrong type arg_int
+            SystemExit(),
+        ),
+    ],
+)
+def test_subclass_tap_simple(
+    data_model: Any, args_string_and_arg_to_expected_value: Tuple[str, Union[Dict[str, Any], BaseException]]
+):
+    _test_subclasser(subclass_tap_simple, data_model, args_string_and_arg_to_expected_value)
+
+
+@pytest.mark.parametrize(
+    "args_string_and_arg_to_expected_value",
+    [
+        (
+            "--arg_int 1 --arg_list x y z",
+            {
+                "arg_int": 1,
+                "arg_bool": True,
+                "arg_list": ["x", "y", "z", "processed"],
+                "argument_with_really_long_name": 3,
+            },
+        ),
+        (
+            "--arg_int 1 --arg_list x y z -arg 2",
+            {
+                "arg_int": 1,
+                "arg_bool": True,
+                "arg_list": ["x", "y", "z", "processed"],
+                "argument_with_really_long_name": 2,
+            },
+        ),
+        (
+            "--arg_int 1 --arg_bool --arg_list x y z --argument_with_really_long_name 2.3",
+            {
+                "arg_int": 1,
+                "arg_bool": False,
+                "arg_list": ["x", "y", "z"],
+                "argument_with_really_long_name": 2.3,
+            },
+        ),
+        # The rest are invalid argument combos, as indicated by the 2nd elt being a BaseException instance
+        (
+            "--arg_list x y z --arg_bool",  # Missing required arg_int
+            SystemExit(),
+        ),
+        (
+            "--arg_int 1 --arg_list x y z -arg not_a_float_or_int",  # Wrong type
+            SystemExit(),
+        ),
+        (
+            "--arg_int 1 --arg_list x y z -arg 5",  # Wrong value arg (aliases argument_with_really_long_name)
+            ValueError("argument_with_really_long_name cannot be > 4"),
+        ),
+    ],
+)
+def test_subclass_tap_complex(
+    data_model: Any, args_string_and_arg_to_expected_value: Tuple[str, Union[Dict[str, Any], BaseException]]
+):
+    # Currently setting test_call=False b/c all data models except the pydantic Model don't accept extra args
+    _test_subclasser(subclass_tap_complex, data_model, args_string_and_arg_to_expected_value, test_call=False)
